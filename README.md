296f572b908162b5e71a1639b5e8059934cc5c7d
DaisukeMatsuura <mail@brightful.jp>
2021-02-22 12:29:31 +0900

    共通Layout作成 & 少しのルーティングを追加

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # React TailwindCSS Sample
 
+## デモページ
+[Next-Tailwind製BLOG](https://happy-banach-3631af.netlify.app/)
+
 ## 技術要件
 ```
 ・React 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,27 @@
-import Head from 'next/head'
+import Layout from '../components/Layout'
 
-export default function Home() {
+const Index = ({ title, description, ...props }) => {
   return (
-    <div className="text-center">
-      <Head>
-        <title>Next.js Blog by TailwindCSS</title>
-        <link rel="icon" href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/271/fire_1f525.png" />
-      </Head>
-      <div className="m-20">
-        Hello World!
-      </div>
-    </div>
-  )
+    <Layout pageTitle={title}>
+       <h1 className="title">Welcome to my blog!</h1>
+       <p className="description">
+         {description}
+       </p>
+       <main>
+         Posts go here!
+       </main>
+     </Layout>)
+  }
+
+export default Index
+
+export async function getStaticProps() {
+  const configData = await import(`../siteconfig.json`)
+
+  return {
+    props: {
+      title: configData.default.title,
+      description: configData.default.description,
+    },
+  }
 }

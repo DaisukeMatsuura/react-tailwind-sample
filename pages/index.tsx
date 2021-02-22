09ac47e7a1252a2b9fc70043f53a6576ff0eb74d
DaisukeMatsuura <mail@brightful.jp>
2021-02-22 10:07:36 +0900

    HelloWorld & デプロイ用コマンド等修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
     <div className="text-center">
+      <Head>
+        <title>Next.js Blog by TailwindCSS</title>
+        <link rel="icon" href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/271/fire_1f525.png" />
+      </Head>
       <div className="m-20">
         Hello World!
       </div>

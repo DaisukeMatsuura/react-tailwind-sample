08ad330ecf07fb2f19ef1b2b8986b741af146a42
DaisukeMatsuura <mail@brightful.jp>
2021-02-25 09:37:06 +0900

    favicon.ico変更

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,6 @@ export default function Layout({ children, pageTitle, ...props }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{pageTitle}</title>
-        <link rel="icon" href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/271/wind-face_1f32c-fe0f.png" />
       </Head>
       <Header />
       <section>

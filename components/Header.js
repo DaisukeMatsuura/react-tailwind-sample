7a2793a2cb5f01a520cfdcd5b8174b1b944d243b
DaisukeMatsuura <mail@brightful.jp>
2021-03-01 10:50:28 +0900

    Header レイアウトの修正

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
       <header className="bg-black h-20">
         <nav className="mb-20 flex items-center h-20">
           <Link href="/">
-            <a className="text-white pl-20 lg:pl-40 xl:pl-64 2xl:pl-80">Brightful Blog</a>
+            <a className="text-white pl-8 md:pl-20 lg:pl-40 xl:pl-64 2xl:pl-80">Brightful Blog</a>
           </Link>
           <Link href="/about">
             <a className="text-white pl-20">About</a>
